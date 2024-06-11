c1739b8bb9e312a7fe70d3e8763c95ec82560c06
Mach3tryhard <matei.sirghe@gmail.com>
2024-06-11 15:46:57 +0300

    test

--- main.js
+++ main.js
@@ -16,15 +16,44 @@ const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshMatcapMaterial( {
 
 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+const cube = new THREE.Mesh( geometry, material);
 
-camera.position.z = 10;
+let vx=[1,1,1,1,1,0,2];
+let vy=[1,1,1,0,2,1,1];
+let vz=[1,0,2,1,1,1,1];
+function Reqcube()
+{
+
+    for(let i=0;i<3;i++)
+    {
+        for(let j=0;j<3;j++)
+        {
+            for(let h=0;h<3;h++)
+            {
+                let apare=true;
+                for(let q=0;q<7;q++)
+                {
+                    if(i==vx[q] && j==vy[q] && h==vz[q])
+                    {
+                        apare=false;
+                    }
+                }
+                if(apare==true)
+                {
+                    const newcube=cube.clone();
+                    newcube.position.set(i,j,h);
+                    scene.add(newcube);
+                }
+            }
+        }
+    }
+}
+
 
 const controls = new OrbitControls( camera, renderer.domElement );
 
 //controls.update() must be called after any manual changes to the camera's transform
-camera.position.set( 0, 20, 10 );
+camera.position.set( 0, 10, 10 );
 controls.update();
 
 function animate() {
@@ -37,3 +66,4 @@ function animate() {
     renderer.render( scene, camera );
 
 }
+Reqcube();
